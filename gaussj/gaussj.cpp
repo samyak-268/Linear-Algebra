127f09d44dc29143ebf9dbf567816572e36818c2
Samyak Datta <datta.samyak@gmail.com>
2014-12-17 22:31:26 +0530

    Gauss elimination (OOP) - First Commit

--- gaussj/gaussj.cpp
+++ gaussj/gaussj.cpp
@@ -1,99 +1,48 @@
 #ifndef _GAUSSJ_CPP
 #define _GAUSSJ_CPP
 
-#include <iostream>
 #include "gaussj.h"
-#define SWAP(a, b) {temp=(a); (a) = (b); (b) = temp;}
+#include <iostream>
 using namespace std;
 
-// helper function for absolute value 
-double fabs(double val) {
-	if (val < 0.0) return -val;
-	else val;
+GaussJ::GaussJ(const Matrix& A_obj, const Matrix& b_obj)
+    : A(A_obj), b(b_obj)
+{
 }
 
-// Gauss-Jordan Elimination
-void GaussJordan(Matrix& A, Matrix& b) {
-/*
-	Solution to linear equations of a matrix 
-	Input : A[0..n-1][0..n-1] , b[0..n-1][0..m-1]
-	Inverse of A and the solutions are stored in
-	the b Matrix itself. 
-*/
-	int n = A.GetNumRows(), m = b.GetNumCols();
-	vector<int> indxc, indxr, ipiv;
-	int i, icol, irow, j, k, l, ll;
-	double big, dum, pivinv, temp;
+void GaussJ::scaleRow(int row_idx, double multiplier)
+{
+    for (int j = 0; j < A.GetNumCols(); ++j)
+        A(row_idx, j) *= multiplier;
+}
 
-	indxr.resize(n);
-	indxc.resize(n);
-	ipiv.resize(n);
+void GaussJ::addRows(int idx_1, int idx_2)
+{
+    for (int j = 0; j < A.GetNumCols(); ++j)
+        A(idx_1, j) = A(idx_1, j) + A(idx_2, j);
+}
 
-	for (i = 0; i < n; i++) {
-		big = 0.0;
-		for (j = 0; j < n; j++) {
-			if (ipiv[j] != 1) {
-				for (k = 0; k < n; k++) {
-					if (ipiv[k] == 0) {
-						if (fabs(A(j, k)) >= big) {
-							big = fabs(A(j, k));
-							irow = j;
-							icol = k;
-						}
-					}
-				}
-			}
-		}
+void GaussJ::swapRows(int idx_1, int idx_2)
+{
+    int n = A.GetNumCols();
+    
+    double temp;
+    for(int j = 0; j < n; ++j)
+    {
+        temp = A(idx_1, j);
+        A(idx_1, j) = A(idx_2, j);
+        A(idx_2, j) = temp;
+    }
+}
 
-		++(ipiv[icol]);
-	/*
-		After getting the pivot element in the previous
-		step, we interchange rows, if needed. For the 
-		ith pivot element indxr[i] and indxc[i] store 
-		the row and column where it was stored originally.
-		If indxr[i] != indxc[i] then there is an implied
-		column interchange. 
-	*/
-		if (irow != icol) {
-			for (l = 0; l < n; l++) SWAP(A(irow, l), A(icol, l)); 
-			for (l = 0; l < m; l++) SWAP(b(irow, l), b(icol, l)); 
-		}
-
-		indxr[i] = irow;
-		indxc[i] = icol;
-		if (A(icol, icol) == 0.0) {
-			cout << "Singular Matrix\n";
-			return;
-		}
-
-		pivinv = 1.0/A(icol, icol);
-		A(icol, icol) = 1.0;
-		for (l = 0; l < n; l++) A(icol, l) *= pivinv;
-		for (l = 0; l < m; l++) b(icol, l) *= pivinv;
-
-		for (ll = 0; ll < n; ll++)
-			if (ll != icol) {
-				dum = A(ll, icol);
-				A(ll, icol) = 0.0;
-				for (l = 0; l < n; l++) A(ll, l) -= A(icol, l)*dum;
-				for (l = 0; l < m; l++) b(ll, l) -= b(icol, l)*dum;
-			}
-
-	}
-
-/*
-	Unscramble the solution in view of column 
-	interchanges. We do this by interchanging
-	pairs of columns in the reverse order that
-	the permuation was built.
-*/
-
-	for (l = n-1; l >= 0; l--) {
-		if (indxr[l] != indxc[l]) {
-			for (k = 0; k < n; k++) 
-				SWAP (A(k, indxr[l]), A(k, indxc[l]));
-		}
-	}
+void GaussJ::displayCoeffMatrix()
+{
+    for(unsigned i = 0; i < A.GetNumRows(); ++i) {
+        for(unsigned j = 0; j < A.GetNumCols(); ++j) {
+            cout << A(i, j) << "  ";
+        }
+        cout << "\n";
+    }
 }
 
 #endif
