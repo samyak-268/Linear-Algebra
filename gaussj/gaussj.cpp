74eeab754199705c4b7b066657a2ac7ceb98d076
Samyak Datta <datta.samyak@gmail.com>
2014-12-18 12:56:37 +0530

    Added method to construct augmented matrix

--- gaussj/gaussj.cpp
+++ gaussj/gaussj.cpp
@@ -5,9 +5,30 @@
 #include <iostream>
 using namespace std;
 
-GaussJ::GaussJ(const Matrix& A_obj, const Matrix& b_obj)
-    : A(A_obj), b(b_obj)
+GaussJ::GaussJ(const Matrix& _A, const Matrix& _b)
+    : A(_A), b(_b)
 {
+}
+
+Matrix GaussJ::augmentMatrix()
+{
+    int N = A.GetNumRows();
+
+    vector<vector<double> > aug_matrix_vec;
+    aug_matrix_vec.resize(N, vector<double>(N+1, 0.0));
+    for(int i = 0; i < N; ++i)
+    {
+        for(int j = 0; j < (N+1); ++j)
+        {
+            if(j == N)
+                aug_matrix_vec[i][j] = b(i, 0);
+            else
+                aug_matrix_vec[i][j] = A(i, j);
+        }
+    }
+
+    Matrix aug(aug_matrix_vec);
+    return aug;
 }
 
 void GaussJ::scaleRow(int row_idx, double multiplier)
@@ -35,11 +56,11 @@ void GaussJ::swapRows(int idx_1, int idx_2)
     }
 }
 
-void GaussJ::displayCoeffMatrix()
+void GaussJ::displayAugMatrix(const Matrix& aug)
 {
-    for(unsigned i = 0; i < A.GetNumRows(); ++i) {
-        for(unsigned j = 0; j < A.GetNumCols(); ++j) {
-            cout << A(i, j) << "  ";
+    for(unsigned i = 0; i < aug.GetNumRows(); ++i) {
+        for(unsigned j = 0; j < aug.GetNumCols(); ++j) {
+            cout << aug(i, j) << "  ";
         }
         cout << "\n";
     }
--- gaussj/gaussj.h
+++ gaussj/gaussj.h
@@ -10,7 +10,7 @@ class GaussJ
          * The data members used in Gauss Elimination.
          * (1) Matrix A (Co-efficient matrix: N X N)
          *      Holds the co-efficients for the system of N linear equations in N unknowns.
-         *  (2) Matrix b (RHS vector: N X 1)
+         * (2) Matrix b (RHS vector: N X 1)
          *      Holds the RHS of the N equations.
          */
         
@@ -19,23 +19,28 @@ class GaussJ
         
         /*
          * The helper functions used for Gauss elimination. All row numberings are 0-based.
-         * (1) void scaleRow(int row_idx, double multiplier)
+         * (1) Matrix augmentMatrix()
+         *      Returns the augmented matrix by concatenating b as a column with A
+         * 
+         * (2) void scaleRow(int row_idx, double multiplier)
          *      Multiplies every element in row_idx-th row with multiplier
          *
-         *  (2) void addRows(int idx_1, int idx_2)
+         * (3) void addRows(int idx_1, int idx_2)
          *      Adds row numbered idx_1 with row numbered idx_2 and saves the result in idx_1
          *
-         *  (3) void swapRows(int idx_1, int idx_2)
+         * (4) void swapRows(int idx_1, int idx_2)
          *      Swaps the contents of the 2 rows numbered idx_1 and idx_2
          */
         
+        Matrix augmentMatrix();
         void scaleRow(int row_idx, double multiplier);
         void addRows(int idx_1, int idx_2);
         void swapRows(int idx_1, int idx_2);
 
+
     public:
-        GaussJ(const Matrix& A_obj, const Matrix& b_obj);
-        void displayCoeffMatrix();
+        GaussJ(const Matrix& _A, const Matrix& _b);
+        void displayAugMatrix(const Matrix& aug);
 };
 
 #endif
